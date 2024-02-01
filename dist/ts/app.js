1d2b59bc2cd155a1b9e2004cf4ea48f420fb3e76
Alexander Pozas <a.posas@outlook.com>
2024-02-01 15:04:31 -0600

    ts classes
    class syntax
    constrcutor
    constructor functions
    this operator

--- dist/ts/app.js
+++ dist/ts/app.js
@@ -1,3 +1,25 @@
 "use strict";
 console.log('Classes and Interfaces');
+/**
+ * Defining a class and an instance
+*/
+class Department {
+    constructor(name) {
+        this.name = name;
+    }
+    // constructor functions
+    describe() {
+        console.log(`Department: ${this.name}`);
+    }
+}
+const accounting = new Department('Accounting');
+console.log(accounting); // object
+accounting.describe();
+// this keyword
+// const accountingCopy = { describe: accounting.describe } // this is refering to accounting copy
+// accountingCopy.describe(); // undefined
+// const accountingCopy = { describe: accounting.describe }; // don´t have the correct type structure
+// accountingCopy.describe(); // error, type Department
+const accountingCopy = { name: 'Some name', describe: accounting.describe };
+accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,1 +1,32 @@
 console.log('Classes and Interfaces');
+
+/**
+ * Defining a class and an instance
+*/
+
+class Department {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  // constructor functions
+  describe(this: Department) {
+    console.log(`Department: ${this.name}`);
+  }
+}
+
+const accounting = new Department('Accounting');
+console.log(accounting); // object
+accounting.describe();
+
+// this keyword
+// const accountingCopy = { describe: accounting.describe } // this is refering to accounting copy
+// accountingCopy.describe(); // undefined
+
+// const accountingCopy = { describe: accounting.describe }; // don´t have the correct type structure
+// accountingCopy.describe(); // error, type Department
+
+const accountingCopy = { name: 'Some name', describe: accounting.describe };
+accountingCopy.describe();
